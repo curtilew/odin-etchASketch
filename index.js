0eff72503ff0f32273e305d0a977feb304f1c2ff
curtilew <182793266+curtilew@users.noreply.github.com>
2025-04-02 18:14:44 -0400

    Reformat divs to create square divs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 for (let i = 0; i < 256; i++) {
     const newDiv = document.createElement("div");
-    newDiv.textContent = `Div ${i+1}`;
+    newDiv.textContent = `Div`;
     newDiv.classList.add("new-div");
     container.appendChild(newDiv);
   }
