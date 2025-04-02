60ffae6751b6c14789e19047720f534e8986e125
curtilew <182793266+curtilew@users.noreply.github.com>
2025-04-02 18:00:19 -0400

    Add 16x16 grid of divs

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const container = document.querySelector('#container');
-
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This is the glorious text-content!";
 
 
-container.appendChild(content);
+for (let i = 0; i < 256; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.textContent = `Div ${i+1}`;
+    newDiv.classList.add("new-div");
+    container.appendChild(newDiv);
+  }
+
+
+
 
 
