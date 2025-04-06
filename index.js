63d5786f3c68a1d481a1d56a055c79e8a467c8ab
curtilew <182793266+curtilew@users.noreply.github.com>
2025-04-06 11:10:18 -0400

    Create a grid that adjusts based on desired size
    Create a grid that maintains its form regardless
    of square size input

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 
 
-for (let i = 0; i < 256; i++) {
-    const newDiv = document.createElement("div");
-    newDiv.textContent = `Div`;
-    newDiv.classList.add("new-div");
-    container.appendChild(newDiv);
-  }
+
+
+// for (let i = 0; i < 256; i++) {
+//     const newDiv = document.createElement("div");
+//     newDiv.textContent = `Div`;
+//     newDiv.classList.add("new-div");
+//     container.appendChild(newDiv);
+//   }
+
+  let userChoice = Number(prompt("How many squares per side for the new grid? "));
+  for (let i = 0; i < userChoice; i++) {
+
+    const rowDiv = document.createElement("div");
+    rowDiv.classList.add("grid-row");
+    rowDiv.style.display = "flex";
+    rowDiv.style.width = "100%";
+
+
+    for (let j = 0; j < userChoice; j++) {
+      const newDiv = document.createElement("div");
+      newDiv.textContent = `Div${i + j}`;
+      newDiv.classList.add("new-div");
+      container.appendChild(newDiv);
+    }
+
+    container.appendChild(rowDiv);
+}
 
 
 
