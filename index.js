7fe00eef3e499a013885daa7a925484b3f877484
curtilew <182793266+curtilew@users.noreply.github.com>
2025-04-07 14:39:17 -0400

    Final Rough Draft
    Add button to create grid size of choice

--- index.js
+++ index.js
@@ -1,42 +1,33 @@
 
-
-
-
-// for (let i = 0; i < 256; i++) {
-//     const newDiv = document.createElement("div");
-//     newDiv.textContent = `Div`;
-//     newDiv.classList.add("new-div");
-//     container.appendChild(newDiv);
-//   }
-
+function createGrid() {
   let userChoice = Number(prompt("How many squares per side for the new grid? "));
+
   for (let i = 0; i < userChoice; i++) {
 
-    const rowDiv = document.createElement("div");
-    rowDiv.classList.add("grid-row");
-    rowDiv.style.display = "flex";
-    rowDiv.style.width = "100%";
-
-  
-
+  const rowDiv = document.createElement("div");
+  rowDiv.classList.add("grid-row");
+  rowDiv.style.display = "flex";
+  rowDiv.style.width = "100%";
 
     for (let j = 0; j < userChoice; j++) {
       const newDiv = document.createElement("div");
       newDiv.textContent = ``;
       newDiv.classList.add("new-div");
       container.appendChild(newDiv);
-    
+
 
     rowDiv.appendChild(newDiv);
   }
-  
 
   container.appendChild(rowDiv);
-}
+  }
 
   const div = document.querySelector("div");
   div.addEventListener("mouseover", function (e) {
-      e.target.style.background = "blue";
-      
-    });
+    e.target.style.background = "blue";
+  });
+
+}
+
+
 
